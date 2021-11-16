8d2f23e162d0120cc74cfc18736074e84df8155b
Kensuke Tozuka <knsktzkml@gmail.com>
2021-11-16 16:53:32 +0900

    Update runsession.py

--- runsession.py
+++ runsession.py
@@ -3,8 +3,7 @@ loadModel( MODEL_FILE )                # Load the model.
 S = createEntityStub( 'Variable:/:S' ) # Create a stub object of 
 
 S[ 'Value' ] = VALUE_OF_S              # Set the value VALUE_OF_S given by the 
-
-model_num = MODEL_NUM                                       # ESM script.
+                                    # ESM script.
 
 run( 200 )                             # Run the simulation for 200 seconds.
 
